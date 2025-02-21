a81fb0e9bd12f32bb3d77b681f74b1eb6e8caeae
GEEKBOBian <ian_smith26@milton.edu>
2025-02-21 13:52:58 -0500

    for loop

--- src/MakingArrays.java
+++ src/MakingArrays.java
@@ -1,39 +1,57 @@
 public class MakingArrays {
 
-    int[] nums = new int[11];
+    int[] nums;
+    public double total;
+
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         MakingArrays make = new MakingArrays();
 
     }
-    public MakingArrays(){
-        System.out.println("Making arrays");
-        nums [0] = 10;
-        nums [1] = 5;
-        nums [2] = nums [1]*3;
-        nums [3] = 2;
-        nums [4] = 4;
-        nums [5] = 100;
-        nums [6] = 68;
-        nums [7] = 30;
-        nums [8] = 79;
-        nums [9] = 37;
-        nums [10] = 43;
 
+
+//making arrays
+    public MakingArrays() {
+        System.out.println("Making arrays");
+        nums = new int[33];
+        for(int x=0;x<nums.length;x=x+1) {
+            nums[x] = (int) (100 * Math.random());
+        }
+        for(int x=0;x<nums.length;x++){
+            total = total+nums[x];
+        }
+//        nums[0] = 10;
+//        nums[1] = 5;
+//        nums[2] = nums[1] * 3;
+//        nums[3] = 2;
+//        nums[4] = 4;
+//        nums[5] = 100;
+//        nums[6] = 68;
+//        nums[7] = 30;
+//        nums[8] = 79;
+//        nums[9] = 37;
+//        nums[10] = 43;
+//calls print
         printArray();
     }
-    public void printArray(){
-        System.out.println(nums [0]);
-        System.out.println(nums [1]);
-        System.out.println(nums [2]);
-        System.out.println(nums [3]);
-        System.out.println(nums [4]);
-        System.out.println(nums [5]);
-        System.out.println(nums [6]);
-        System.out.println(nums [7]);
-        System.out.println(nums [8]);
-        System.out.println(nums [9]);
-        System.out.println(nums [10]);
+//prints out code
+    public void printArray() {
+        for (int x = 0; x < 33; x++) {
+            System.out.println(nums[x]);
+//        System.out.println(nums [1]);
+//        System.out.println(nums [2]);
+//        System.out.println(nums [3]);
+//        System.out.println(nums [4]);
+//        System.out.println(nums [5]);
+//        System.out.println(nums [6]);
+//        System.out.println(nums [7]);
+//        System.out.println(nums [8]);
+//        System.out.println(nums [9]);
+//        System.out.println(nums [10]);
 
+        }
+        System.out.println("total " + total);
     }
 }
