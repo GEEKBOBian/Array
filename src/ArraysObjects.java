ca3f040cbcdf0f01c65ea42ecf4c646959bce697
GEEKBOBian <ian_smith26@milton.edu>
2025-03-05 11:26:02 -0500

    did more stuff

--- src/ArraysObjects.java
+++ src/ArraysObjects.java
@@ -8,10 +8,33 @@ public class ArraysObjects {
 
     public ArraysObjects(){
         System.out.println("hehe");
-        Sushi s = new Sushi();
-        soosh[0] = s;
-        soosh[0].printInfo();
+//        Sushi s = new Sushi();
+//        soosh[0] = s;
+//        soosh[0].printInfo();
 
+        Sushi s1 = new Sushi(10);
+        soosh[1] = s1;
+        soosh[1].printInfo();
+
+        for(int x=0 ; x<10 ; x++){
+            soosh[x] = new Sushi(Math.random()*250);
+            //soosh[x].printInfo();
+
+        }
+        printArray();
+
+    }
+
+    public void printArray(){
+        for(int x=0 ; x< soosh.length ; x++){
+            soosh[x].printInfo();
+        }
+
+    }
+    public void chageSize(){
+        for(int x=0;x<soosh.length; x++){
+            soosh[x].size = (int)(Math.random() *10)+ 10;
+        }
     }
 
 }
